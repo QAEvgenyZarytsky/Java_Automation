688bb20039b25a310567d9a8d571d5f944fb7f6e
Evgeniy <qa.zarytsky@gmail.com>
2022-08-03 18:16:20 +0300

    Correction HomeWork3

---
+++ src/HomeWork3_correction.java
@@ -0,0 +1,95 @@
+public class HomeWork3_correction {
+    public static void main(String[] args) {
+        //Exercise 1;
+
+//        Scanner input = new Scanner(System.in);
+//        String exit = "Exit";
+//        String yourText;
+//
+//        do {
+//            System.out.println("Please enter your world: ");
+//            yourText = input.next();
+//
+//        } while (!yourText.equals(exit));
+//        System.out.println("The program is stopped!");
+
+        // ---------------------------------------------------
+
+        // Exercise 2;
+
+//        Scanner input = new Scanner(System.in);
+//
+//        do {
+//            System.out.print("Please enter a number: ");
+//            int sum = 0;
+//            int x = input.nextInt();
+//
+//            if (x >= 0) {
+//                for (int i = 0; i <= x; i++) {
+//                    sum = sum + i;
+//                }
+//            }
+//            else {
+//                for (int i = 0; x <= i; x++) {
+//                    sum = sum + x;
+//                }
+//            }
+//
+//           System.out.println("Result: " + sum );
+//             break;
+//
+//        } while (true);
+
+        // ---------------------------------------------------
+
+        //Exercise 3;
+
+//        do{
+//            System.out.println("Please enter your score: ");
+//            Scanner input = new Scanner(System.in);
+//            int score = input.nextInt();
+//
+//                while (score < 0 || score > 100) {
+//                    System.out.println("You are entering an invalid value!!!");
+//                    System.out.println("Please enter a valid value! ");
+//                    score = input.nextInt();
+//                }
+//
+//                if (score >= 70) {
+//                System.out.println("Congrats! You've passed the test!");
+//
+//                } else {
+//                System.out.println("Sorry, you've failed the test!");
+//            }
+//
+//            break;
+//        } while (true);
+
+        //-----------------------------------------------------------------
+
+        // Exercise 4;
+//        Scanner input = new Scanner(System.in);
+//        String[] channels = {"0.EXIT","1.ONT", "2.BT", "3.NTV", "4.TNT", "5.SPORT", "6.Evronews", "7.AUTO+",
+//                "8.OceanTV", "9.TV-1000"};
+//
+//
+//        int numberOfChannel;
+//        do {
+//            System.out.println("Please choose number of channel!\n");
+//            for (String list : channels) {
+//                System.out.println(list);
+//            }
+//            numberOfChannel = input.nextInt();
+//
+//            for (int i = 1; i <= channels.length; i++) {
+//
+//                if (numberOfChannel == i) {
+//                    System.out.println("Your watching: " + channels[i]);
+//
+//                }
+//            }
+//
+//        } while (numberOfChannel != 0);
+
+    }
+}
